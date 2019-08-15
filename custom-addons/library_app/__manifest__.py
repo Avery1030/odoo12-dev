501f54a9a5f38baa3f6174513c4f58cf47d174fd
yizhihuangege <42306039@qq.com>
2019-08-15 10:40:20 +0800

    updated

--- custom-addons/library_app/__manifest__.py
+++ custom-addons/library_app/__manifest__.py
@@ -1,3 +1,14 @@
 {
-    'title':'library app',
+    'name':'Library Management',
+    'description':'Manage library book catalogue and lending.',
+    'author':'wangshuai',
+    'depends':['base'],
+    'application':True,
+    'data':[
+        'security/library_security.xml',
+        'security/ir.model.access.csv',
+        'views/library_menu.xml',
+        'views/book_view.xml',
+        'views/book_list_template.xml',
+    ]
 }
---
+++ custom-addons/library_app/models/res_partner.py
@@ -0,0 +1,12 @@
+from odoo import fields, models
+
+
+class Partner(models.Model):
+    _inherit = 'res.partner'
+    book_ids = fields.Many2many(
+        'library.book', string='Authored Books')
+    published_book_ids = fields.One2many(
+        'library.book',  # related model
+        'publisher_id',  # fields for 'this' on related model
+        string='Published Books'
+    )

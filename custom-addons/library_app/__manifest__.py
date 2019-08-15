2351d2a63249b2d6f6361a1622f7305fd07cab05
yizhihuangege <42306039@qq.com>
2019-08-15 17:18:21 +0800

    开发报表和服务端Qweb

--- custom-addons/library_app/__manifest__.py
+++ custom-addons/library_app/__manifest__.py
@@ -1,14 +1,15 @@
 {
-    'name':'Library Management',
-    'description':'Manage library book catalogue and lending.',
-    'author':'wangshuai',
-    'depends':['base'],
-    'application':True,
-    'data':[
+    'name': 'Library Management',
+    'description': 'Manage library book catalogue and lending.',
+    'author': 'wangshuai',
+    'depends': ['base'],
+    'application': True,
+    'data': [
         'security/library_security.xml',
         'security/ir.model.access.csv',
         'views/library_menu.xml',
         'views/book_view.xml',
         'views/book_list_template.xml',
+        'reports/library_book_report.xml',
     ]
 }

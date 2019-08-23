fedc3eaf209a567ce9fb280efed9a9a56929c08c
yizhihuangege <42306039@qq.com>
2019-08-23 15:57:08 +0800

    add Openworx Material Backend Theme V12

---
+++ custom-addons/my_theme/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name':'Tutorial Theme',
+    'description':'A description for your theme.',
+    'version':'1.0',
+    'author':'Your name',
+
+    'data':[
+        'views/layout.xml',
+        'views/pages.xml',
+        'views/assets.xml',
+        'views/snippets.xml',
+        'views/option.xml',
+    ],
+    'category':'Theme/Creative',
+    'depends':[
+        'website',
+        'website_blog',
+        'sale',
+    ]
+}
---
+++ custom-addons/my_theme/static/src/js/tutorial_editor.js
@@ -0,0 +1,12 @@
+(function() {
+    'use strict';
+
+    var website = odoo.website;
+    console.log(odoo);
+//    website.odoo_website = {};
+//    website.snippet.options.snippet_testimonial_options = website.snippet.Option.extend({
+//       onFocus: function() {
+//           console.log("On focus!");
+//       }
+//    })
+})();
